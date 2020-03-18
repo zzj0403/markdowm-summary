303811139aee610586eeb7e6e44b7619e7205aff
Your Name <you@example.com>
2020-03-18 19:44:16 +0800

    1

--- log/conf/config.py
+++ log/conf/config.py
@@ -5,6 +5,5 @@ USERNAME = '1cloud'
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 KEY = os.path.join(BASE_DIR, 'files', '1cloud')
 HOST = {
-    'beiyu': ['121.40.230.24', '114.55.174.211', '47.99.164.167'],
-    # 'ss': ['47.97.73.36', '47.99.129.161', '47.99.63.207']
+
 }
